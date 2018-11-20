6304f35839702f21299a6038730a8e468a8dc002
Dell <youssifhamdy83@yahoo.com>
2018-11-20 21:33:05 +0200

    initial commit

--- app/src/main/java/com/youssif/joe/weatherapptask/FirstActivity.java
+++ app/src/main/java/com/youssif/joe/weatherapptask/FirstActivity.java
@@ -1,5 +1,8 @@
 package com.youssif.joe.weatherapptask;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,17 +14,25 @@ import com.youssif.joe.weatherapptask.Adapter.WeatherCitiesAdapter;
 import com.youssif.joe.weatherapptask.Commen.Common;
 import com.youssif.joe.weatherapptask.Model.WeatherCitiesResult;
 import com.youssif.joe.weatherapptask.Retrofit.IOpenWeatherMap;
-import com.youssif.joe.weatherapptask.Retrofit.RetrofitClient;
+
+import java.io.IOException;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.Cache;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class FirstActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    int cacheSize = 10 * 1024 * 1024;
 
     CompositeDisposable compositeDisposable;
     IOpenWeatherMap mService;
@@ -40,16 +51,60 @@ public class FirstActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL , false));
         
-        compositeDisposable = new CompositeDisposable();
-        Retrofit retrofit = RetrofitClient.getInstance();
-        mService = retrofit.create(IOpenWeatherMap.class);
+
 
 
         getCitiesWeatherInformation();
         
     }
 
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+
+
+
     private void getCitiesWeatherInformation() {
+
+
+        Cache cache = new Cache(getCacheDir(), cacheSize);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .cache(cache)
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public okhttp3.Response intercept(Interceptor.Chain chain)
+                            throws IOException {
+                        Request request = chain.request();
+                        if (!isNetworkAvailable()) {
+                            int maxStale = 60 * 60 * 24 * 28; // tolerate 4-weeks stale \
+                            request = request
+                                    .newBuilder()
+                                    .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                                    .build();
+                        }
+                        return chain.proceed(request);
+                    }
+                })
+                .build();
+
+
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("https://api.openweathermap.org/data/2.5/")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create());
+
+
+        compositeDisposable = new CompositeDisposable();
+        Retrofit retrofit = builder.build();
+        mService = retrofit.create(IOpenWeatherMap.class);
+
         compositeDisposable.add(mService.getCitiesWeatherByID(
                 "524901,703448,2643743,4396915,6155070" ,
                 Common.APP_ID,
